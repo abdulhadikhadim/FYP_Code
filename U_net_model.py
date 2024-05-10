54e0958e952bbbbddac39e22b1405efd328e3e7a
Abdul Hadi <142796091+abdulhadikhadim@users.noreply.github.com>
2024-05-10 12:50:38 +0500

    Unet_model.py
    Code for the multiclass and use softmax function instead of sigmoid

--- U_net_model.py
+++ U_net_model.py
@@ -1,9 +1,8 @@
-
 from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
 from tensorflow.keras.models import Model
 
-def conv_block(input, num_filters):
-    x = Conv2D(num_filters, 3, padding="same")(input)
+def conv_block(inputs, num_filters):
+    x = Conv2D(num_filters, 3, padding="same")(inputs)
     x = BatchNormalization()(x)
     x = Activation("relu")(x)
 
@@ -13,26 +12,26 @@ def conv_block(input, num_filters):
 
     return x
 
-
-def encoder_block(input, num_filters):
-    x = conv_block(input, num_filters)
+def encoder_block(inputs, num_filters):
+    x = conv_block(inputs, num_filters)
     p = MaxPool2D((2, 2))(x)
     return x, p
 
-
-def decoder_block(input, skip_features, num_filters):
-    x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(input)
+def decoder_block(inputs, skip_features, num_filters):
+    x = Conv2DTranspose(num_filters, 2, strides=2, padding="same")(inputs)
     x = Concatenate()([x, skip_features])
     x = conv_block(x, num_filters)
     return x
 
-def build_unet(input_shape):
+def build_unet(input_shape, num_classes):
     inputs = Input(input_shape)
 
     s1, p1 = encoder_block(inputs, 64)
     s2, p2 = encoder_block(p1, 128)
     s3, p3 = encoder_block(p2, 256)
     s4, p4 = encoder_block(p3, 512)
+
+
 
     b1 = conv_block(p4, 1024)
 
@@ -41,13 +40,12 @@ def build_unet(input_shape):
     d3 = decoder_block(d2, s2, 128)
     d4 = decoder_block(d3, s1, 64)
 
-    outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d4)
+    outputs = Conv2D(num_classes, 1, padding="same", activation="softmax")(d4)
 
-    model = Model(inputs, outputs, name="U-Net")
+    model = Model(inputs, outputs, name="UNET")
     return model
 
-
 if __name__ == "__main__":
-    input_shape = (512, 512, 3)
-    model = build_unet(input_shape)
+    input_shape = (640, 640, 3)
+    model = build_unet(input_shape,3)
     model.summary()
